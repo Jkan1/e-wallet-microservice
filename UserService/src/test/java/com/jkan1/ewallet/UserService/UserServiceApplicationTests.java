18429777cbf37d29c1007fae121671eb4b1be6bb
JKan1 <kan.joshi.1@gmail.com>
2020-09-23 18:42:00 +0530

    Initial Commit

---
+++ UserService/src/test/java/com/jkan1/ewallet/UserService/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkan1.ewallet.UserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ WalletService/src/test/java/com/jkan1/ewallet/WalletService/WalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkan1.ewallet.WalletService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
